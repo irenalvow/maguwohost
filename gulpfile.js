9aa402aac5df6c6c4cef8c79281adaac8456a4f8
Dan Green <danon0404@gmail.com>
2017-02-21 20:23:08 +0700

    v0.2.1

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@
  * Requirements
  */
 
-const { notify, reportError, extendGulpConnect } = require('./helpers'),
+const { notify, reportError } = require('./helpers'),
 	gulp    = require('gulp'),
 	gutil   = require('gulp-util'),
 	replace = require('gulp-replace'),
@@ -25,13 +25,7 @@ const { notify, reportError, extendGulpConnect } = require('./helpers'),
 	sass      = require('gulp-sass'),
 	styleLint = require('gulp-stylelint'),
 
-	webpack = require('webpack'),
-	esLint  = require('gulp-eslint'),
-
-	pkg = require('./package.json'),
-	wpk = require('./webpack.config');
-
-extendGulpConnect(connect);
+	pkg = require('./package.json');
 
 /**
  * Configs
@@ -40,13 +34,9 @@ extendGulpConnect(connect);
 const { browsers } = pkg;
 
 const paths = {
-	html:    'src/*.html',
-	images:  'src/images/**/*.{jpg,webp,png,svg,gif}',
-	styles:  'src/app/**/*.scss',
-	scripts: [
-		'src/app/main.js',
-		'src/app/**/*.js'
-	]
+	html:   'src/*.html',
+	images: 'src/images/**/*.{jpg,webp,png,svg,gif}',
+	styles: 'src/app/**/*.scss'
 };
 
 /**
@@ -161,85 +151,13 @@ gulp.task('style:build', gulp.series('style:lint', () =>
 ));
 
 /**
- * Webpack compilers
- */
-
-const webpackDevCompiler = webpack(wpk.dev(paths.scripts[0], 'dist/app')),
-	webpackBuildCompiler = webpack(wpk.build(paths.scripts[0], 'dist/app'));
-
-/**
- * JavaScript tasks
- */
-
-gulp.task('script:watch', (done) => {
-	gulp.watch(paths.scripts, gulp.series('script:dev'));
-	done();
-});
-
-gulp.task('script:lint', () =>
-	gulp.src(paths.scripts)
-		.pipe(esLint())
-		.pipe(esLint.format())
-		.pipe(esLint.failAfterError())
-		.on('error', reportError)
-);
-
-gulp.task('script:dev', gulp.parallel('script:lint', done =>
-	webpackDevCompiler.run((error, stats) => {
-
-		if (error) {
-			notify.onError(error);
-			return done();
-		}
-
-		if (stats.hasErrors()) {
-			notify.onError(new Error('Webpack compilation is failed.'));
-		} else {
-			connect.changed(webpackDevCompiler.options.entry);
-			notify('Scripts are updated.', true);
-		}
-
-		gutil.log(`${gutil.colors.cyan('webpack')}:`, `\n${stats.toString({
-			chunks: false,
-			colors: true
-		})}`);
-
-		return done();
-	})
-));
-
-gulp.task('script:build', gulp.series('script:lint', done =>
-	webpackBuildCompiler.run((error, stats) => {
-
-		if (error) {
-			notify.onError(error);
-			return done();
-		}
-
-		if (stats.hasErrors()) {
-			notify.onError(new Error('Webpack compilation is failed.'));
-		} else {
-			notify('Scripts are updated.', true);
-		}
-
-		gutil.log(`${gutil.colors.cyan('webpack')}:`, `\n${stats.toString({
-			chunks: false,
-			colors: true
-		})}`);
-
-		return done();
-	})
-));
-
-/**
  * Main tasks
  */
 
 gulp.task('watch', gulp.parallel(
 	'html:watch',
 	'images:watch',
-	'style:watch',
-	'script:watch'
+	'style:watch'
 ));
 
 gulp.task('server', (done) => {
@@ -257,8 +175,7 @@ gulp.task('dev', gulp.series(
 			'style:dev',
 			'html:dev'
 		),
-		'images:dev',
-		'script:dev'
+		'images:dev'
 	),
 	'watch'
 ));
@@ -268,8 +185,7 @@ gulp.task('build', gulp.parallel(
 		'style:build',
 		'html:build'
 	),
-	'images:build',
-	'script:build'
+	'images:build'
 ));
 
 gulp.task('default', gulp.series('dev'));
